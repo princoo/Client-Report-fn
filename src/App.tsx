73ede7dccb638b3a4a3124cf21badc2f751089b5
princoo <princeineza@gmail.com>
2024-02-21 14:29:38 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import NavBar from './components/NavBar';
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <NavBar />
+      {/* <Routes>
+        <Route path="/" element={}></Route>
+      </Routes> */}
+    </div>
+  );
+}
+
+export default App;
