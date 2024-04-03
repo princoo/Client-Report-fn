5ffded42ed80dfa1eb16b4d20984a8a541bb9898
princoo <princeineza@gmail.com>
2024-04-03 11:20:23 +0200

    refactoring errorBaundaries

---
+++ src/utils/handleRefresh.tsx
@@ -0,0 +1,5 @@
+export const handleReset = () => {
+  // Perform any actions needed to reset state or recover from the error
+  // For example, you might navigate the user to a different page or reload the current page
+  window.location.reload(); // Example: Reload the current page
+};
